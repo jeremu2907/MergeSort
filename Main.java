64e8a8f46be5dc0481c70a1ada23a29c6babdb91
jeremu2907 <jeremu2907@gmail.com>
2022-02-27 13:42:50 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,15 +22,34 @@ public class Main{
         int midPoint = arr.length / 2;
         int endPoint = arr.length - midPoint;
 
+            //Splitting array
         int leftArr[] = new int[midPoint];
         int rightArr[] = new int[endPoint];
+        for(int i = 0; i < midPoint; i++){
+            leftArr[i] = arr[i];
+        }
+        for(int i = 0; i + endPoint < arr.length; i++){
+            rightArr[i] = arr[i + endPoint];
+        }
 
             //Recursive step
         sort(leftArr);
         sort(rightArr);
         
+        //Merge sort;
+        int merged[] = new int[midPoint + endPoint];
+        int leftPtr = 0, rightPtr = 0;
+        for(int i = 0; i < merged.length; i++){
+            if(leftArr[leftPtr] >= rightArr[rightPtr]){
+                merged[i] = rightArr[rightPtr];
+                if(rightPtr < rightArr.length) rightPtr++;
+            }
+            else{
+                merged[i] = leftArr[leftPtr];
+                if(leftPtr < leftArr.length) leftPtr++;
+            }
+        }
 
+        return;
     }
-
-    private static void swap(){}
 }
